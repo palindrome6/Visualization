c81caf3fdb72f264ac4322faa4570b13fb29e0e7
Nitin Narayan <nitinnarayan1501@gmail.com>
2016-01-23 18:52:57 +0100

    Basic button to select years 90,91,92

--- Project/Python json/fill_key_addjson.py
+++ Project/Python json/fill_key_addjson.py
@@ -4,22 +4,34 @@ with open("INTERNET_Q.json") as json_file:
     json_data = json.load(json_file)
     print(json_data)
 for key in json_data:
-    if json_data[key]["2010"]>=60:
-        json_data[key]['fillKey']='HIGH'
-    elif json_data[key]["2010"]>=40:
-        json_data[key]['fillKey']='MEDIUM'
+    if json_data[key]["1992"]<=5:
+        json_data[key]['fillKey']='LEQ5'
+    elif json_data[key]["1992"]<=10:
+        json_data[key]['fillKey']='LEQ10'
+    elif json_data[key]["1992"]<=20:
+        json_data[key]['fillKey']='LEQ20'
+    elif json_data[key]["1992"]<=30:
+        json_data[key]['fillKey']='LEQ30'
+    elif json_data[key]["1992"]<=40:
+        json_data[key]['fillKey']='LEQ40'
+    elif json_data[key]["1992"]<=50:
+        json_data[key]['fillKey']='LEQ50'
+    elif json_data[key]["1992"]<=60:
+        json_data[key]['fillKey']='LEQ60'
+    elif json_data[key]["1992"]<=70:
+        json_data[key]['fillKey']='LEQ70'
     else:
-        json_data[key]['fillKey']='LOW'
+        json_data[key]['fillKey']='LEQ100'
 for key in json_data:
     print json_data[key]["fillKey"]
 
 print(json_data)
 
-with open('INTERNET_R.json', 'w') as outfile:
+with open('INTERNET_1992.json', 'w') as outfile:
     json.dump(json_data, outfile)
 # i=0
 # for key,vslue in json_data:
-#     if key.1990>=60:
+#     if key.1992>=60:
 #         key[fillKey]='HIGH'
 #     elif key.value>=40:
 #         key[fillKey]='MEDIUM'
